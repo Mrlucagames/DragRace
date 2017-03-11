d19b7f42782b996aa84eafc9b1aa97fe98c11adf
Runar Fra <runar@tuta.io>
2017-03-11 17:05:56 +0100

    The Main scene includes a playable song, Peanut Butter

--- unity/Assets/Scripts/Activator.cs
+++ unity/Assets/Scripts/Activator.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class Activator : MonoBehaviour {
 
+	SpriteRenderer sr;
 	public KeyCode key;
 	bool active = false;
 	GameObject note;
+	Color old;
+
+	public bool createMode;
+	public GameObject n;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+		sr = GetComponent<SpriteRenderer> ();
+	}
+
+	void Start() {
+		old = sr.color;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (key) && active) {
-			Destroy (note);
+		if (createMode && Input.GetKeyDown (key)) {
+			if (Input.GetKeyDown (key)) {
+				Instantiate (n, transform.position, Quaternion.identity);
+			}
+		} else {
+			if (Input.GetKeyDown (key)) {
+				StartCoroutine (Pressed ());
+			}
+			if (Input.GetKeyDown (key) && active) {
+				Destroy (note);
+				AddScore ();
+				active = false;
+			}
 		}
+
+
 	}
 
 	void OnTriggerEnter2D(Collider2D col)
@@ -33,4 +55,16 @@ public class Activator : MonoBehaviour {
 	{
 		active = false;
 	}
+
+	void AddScore() {
+		PlayerPrefs.SetInt ("Score", PlayerPrefs.GetInt ("Score") + 100);
+	}
+
+	IEnumerator Pressed () {
+		sr.color = new Color (0, 0, 0);
+		yield return new WaitForSeconds (0.1f);
+		sr.color = old;
+
+	}
+
 }
